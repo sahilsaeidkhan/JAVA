90881c005e19f3a35f62fddc2ab70b3f60007af9
sahil <sahilsaeidkhan610@gmail.com>
2025-08-29 00:16:58 +0530

    recursion

---
+++ fiborecursion.java
@@ -0,0 +1,22 @@
+// fibonacci series simple question
+
+import java.util.Scanner;
+
+public class fiborecursion {
+   public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the nth number you want to print of fibonacci series ");
+    int n = sc.nextInt();
+
+    int fibono = fib(n);
+    System.out.println(fibono);
+   } 
+
+   static int fib(int n){
+    if ( n < 2 ){
+        return n;
+    }
+    return fib(n-1) + fib(n-2);
+   }
+}
