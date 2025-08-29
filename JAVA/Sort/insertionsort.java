833a4d694e572cd29a6dbaa50c65caac14106bb8
sahil <sahilsaeidkhan610@gmail.com>
2025-08-29 20:22:17 +0530

    files added

---
+++ JAVA/Sort/insertionsort.java
@@ -0,0 +1,26 @@
+package JAVA.Sort;
+import java.util.Arrays;
+
+public class insertionsort {
+    public static void main(String[] args) {
+        int arr[] = {2,1,6,5,11,0,-4};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void sort(int[] arr){
+        for ( int i = 0; i<arr.length-1;i++){
+            for ( int j = i+1;j>0;j--){
+                if ( arr[j] < arr[j-1]){
+                    swap(arr, j ,  j-1);
+                }
+                else {
+                    break;
+                }
+            }
+        } }
+           static void swap(int[] arr , int first , int second){
+                  int temp = arr[first];
+                  arr[first] = arr[second];
+                  arr[second] = temp;
+        }
+}
