740132a4601863cf7a027a14f4be1f1af5f664ad
sahil <sahilsaeidkhan610@gmail.com>
2025-09-01 11:27:39 +0530

    bitwise changes

--- JAVA/Bitwise.java/unique.java
+++ JAVA/Bitwise.java/unique.java
@@ -1,6 +1,13 @@
 public class unique {
     public static void main(String[] args){
-        int[] uniqueno = { 1 ,3,5,3,6,7,1,5,6};
-         
+        int[] uniqueno = {3,5,3,6,7,7,1,5,6};
+        System.out.println(unique(uniqueno));
     }
+    static int unique(int[] uniqueno){
+           int sum = 0;
+        for ( int i = 0 ; i<uniqueno.length;i++){
+            sum = sum ^ uniqueno[i];
+        } 
+         return sum;
+    }  
 }
