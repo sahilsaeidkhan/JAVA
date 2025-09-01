713797c8f5bcd0723581873582b1075e93597c1f
sahil <sahilsaeidkhan610@gmail.com>
2025-09-01 11:14:33 +0530

    updating methods

---
+++ JAVA/Methods/sum.java
@@ -0,0 +1,17 @@
+package Methods;
+
+public class sum {
+    class Main{
+    static int logicc(int x, int y){
+        int z;
+        z = x + y;
+        return z;
+    }
+     public static void main(String args []){
+         int a = 67; int b = 5;
+         int c ;
+         c = logicc(a,b);
+         System.out.print( "Sum is " + c);
+     }
+ }
+}
