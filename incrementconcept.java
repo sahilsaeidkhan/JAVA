f668cce72449d49187fc14b69b31f9e31123b17d
Sahil Saeid <sahilsaeidkhan610@gmail.com>
2025-09-22 12:09:39 +0530

    a

---
+++ incrementconcept.java
@@ -0,0 +1,14 @@
+// the code will go infinite as n-- ( pass first the reduce ) , so we have to use --n
+
+public class incrementconcept {
+    public static void main(String[] args) {
+        System.out.println(decrement(5));
+    }
+    public static int decrement(int n){
+        if ( n == 0){
+            return 0 ;
+        }
+        System.out.println(n);
+         return decrement(n--);
+    }
+}
