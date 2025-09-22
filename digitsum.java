ff632c9c8f99c092b5b1d8c90e818efe2b2c74ce
Sahil Saeid <sahilsaeidkhan610@gmail.com>
2025-09-22 11:44:56 +0530

    files added

---
+++ digitsum.java
@@ -0,0 +1,12 @@
+public class digitsum {
+    public static void main(String[] args) {
+        int n = 123456;
+        System.out.print( digit(n));
+    }
+    public static int digit(int n){
+      if ( n == 0){
+        return 0;
+      }
+      return digit(n/10) + ( n%10);
+    }
+}
