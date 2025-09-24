eaec84ba749cdaecd4d8ebe4584ede22a8898ea0
Sahil Saeid <sahilsaeidkhan610@gmail.com>
2025-09-24 10:05:49 +0530

    files added

---
+++ Numtraingle.java
@@ -0,0 +1,13 @@
+
+public class Numtraingle {
+    public static void main(String[] args) {
+        int a = 1;
+        for ( int i = 0;i<5;i++){
+            for ( int j = 0;j<=i;j++){
+                System.out.print(a + " ");
+                a++;
+            }
+             System.out.println();
+        }
+    }
+}
