c1dfddd5ce46c44c90bb912c1f32f9f8efe14b9a
Sahil Saeid <sahilsaeidkhan610@gmail.com>
2025-10-07 11:25:41 +0530

    oops

---
+++ JAVA/OOPS/first.java
@@ -0,0 +1,10 @@
+public class first {
+      static class Student{
+            int rollno = 5;
+            String name;
+        }
+    public static void main(String[] args) {
+       Student Kunal = new Student();
+            System.out.print( Kunal.rollno);
+    }      
+}
